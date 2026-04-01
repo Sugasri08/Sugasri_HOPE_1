b62bf49346c564615220a2ee503cd5c8c5690c9b
Sugasri08 <sugasrivenkat08@gmail.com>
2026-04-01 21:05:50 +0530

    uploaded day 11 task

--- DAY_11/com/ecommerce/model/Product.java
+++ DAY_11/com/ecommerce/model/Product.java
@@ -4,7 +4,7 @@ public class Product {
     private int id;
     private String name;
     private int price;
-    public Product(int id, String name, int price){
+    public Product(int id,String name, int price){
         this.id=id;
         this.name=name;
         this.price=price;
